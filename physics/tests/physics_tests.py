02e9c4f49c91be363e21877d3d671dabbf593477
chuphay <chuphay.gmail.com>
2013-12-27 15:08:14 +0800

    millikan and least

--- physics/tests/physics_tests.py
+++ physics/tests/physics_tests.py
@@ -1,6 +1,7 @@
 from nose.tools import *
 import physics.Aitken 
 import physics.aitken
+import physics.first_least_square as fls
 pnts_1 = [0.0,0.5,1.0]
 fn_1 = [0.0,0.5,1.0]
 pnts_2 = [0.0,0.5,1.0,1.5,2.0]
@@ -14,4 +15,16 @@ def test_aitken_method():
 def test_aitken_upNdown():
     assert_equal(physics.aitken.upNdown(0.9,pnts_1,fn_1),0.9)
     assert_equal(round(physics.aitken.upNdown(0.9,pnts_2,fn_2),6),0.807473)  
+def test_fls():
+    x_in = [4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+    dat = [6.558,8.206,9.88,11.5,13.14,14.82,16.4,18.04,19.68,21.32,22.96,24.6,26.24,27.88,29.52]
+    u = fls.method(21,x_in,dat)
+    n = len(dat)-1
+    sum = 0
+    for i in range(len(dat)):   
+        sum += x_in[i]
+    e = u[1][n+1]
+    dq = u[0][n+1]-u[1][n+1]*sum/(n+1)
+    assert_equal(round(e,2),1.64)  
+    assert_equal(round(dq,2),0.03)    
       
