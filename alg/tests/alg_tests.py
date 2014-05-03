44c281512fde0983fa00d3723042ee6de7ec334f
chuphay <chuphay.gmail.com>
2014-05-03 18:37:13 +0800

    sort

--- alg/tests/alg_tests.py
+++ alg/tests/alg_tests.py
@@ -1,6 +1,7 @@
 from nose.tools import *
 import random
 import week1.multiply
+import week1.my_sort
 
 def test_grade_school_simple():
     for i in range(10):
@@ -11,4 +12,12 @@ def test_grade_school_hard():
         A = random.randint(1,10000000)
         B = random.randint(1,10000000)
         assert_equal(week1.multiply.grade_school(A,B),A*B)
+        
+def test_sort():
+    a = [i for i in xrange(100000)]
+    random.shuffle(a)
+    b = [i for i in xrange(900)]
+    random.shuffle(b)
+    assert_equal(week1.my_sort.insert_sort(b),sorted(b))
+    assert_equal(week1.my_sort.merge_sort(a),sorted(a))       
   
---
+++ alg/week1/my_sort.py
@@ -0,0 +1,46 @@
+def insert(sorted_list,number):
+    """helper function for insert_sort"""
+    if len(sorted_list) == 0:
+        return [number]
+    elif sorted_list[-1] <= number:
+        sorted_list.append(number)
+        return sorted_list
+    else:
+        return insert(sorted_list[:-1],number)+sorted_list[-1:]
+
+def insert_sort(unsorted_list):
+    """can't sort lists that are over approximately 1000 numbers long
+    due to reaching python's recursion limit""" 
+    sorted_list = []
+    for i in range(len(unsorted_list)):
+        sorted_list = insert(sorted_list,unsorted_list[i])
+    return sorted_list  
+    
+def merge_sort(l):
+    n = len(l)
+    if n>1:
+        A, B = merge_sort(l[:n/2]), merge_sort(l[n/2:])
+        l = []
+        try:
+            for k in xrange(n):
+                if A[0]<B[0]:
+                    l.append(A.pop(0))
+                else:
+                    l.append(B.pop(0))
+        except IndexError:
+            l += A + B
+    return l         
+
+if __name__ == '__main__':
+    l = [2,1,4,3,7,10,4]
+    print insert_sort(l)
+    print sorted(l)
+    from random import shuffle
+    a = [i for i in xrange(1000000)]
+    shuffle(a)
+    b = [i for i in xrange(1000)]
+    shuffle(b)
+    print a[:10]
+    print l
+    print insert_sort(b) == sorted(b)
+    print merge_sort(a) == 2
